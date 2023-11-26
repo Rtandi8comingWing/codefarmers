a6b4e7419bf202a28030cd2b82a9fcc73591fb56
AlchemyHAN <alchemylinux@gmail.com>
2023-11-27 02:21:27 +0800

    替换Alchemy图片

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const service = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // easy-mock服务挂了，暂时不使用了
-    baseURL: 'http://10.69.206.242',
+    baseURL: 'http://codefarmers.swu.social',
     timeout: 5000,
     withCredentials: true
 });
